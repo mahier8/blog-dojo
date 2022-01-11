29fa48a584f6844364899228ff740236ba9cd766
Mahier <mahier.sydow8@gmail.com>
2022-01-11 17:06:16 +0900

    added in delete functionality and rendered lists through props

---
+++ src/BlogList.js
@@ -0,0 +1,25 @@
+const BlogList = ({blogs, title, handleDelete}) => {
+
+// const BlogList = ({blogs, title}) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+    // a better way to do the above is at line 1
+
+
+
+    return ( 
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+            </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,43 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-  const handleClick = () => {
-    console.log('hello, ninjas');
+  // let name = 'mario';
+  // const [name, setName] = useState('mario');
+  // const [age, setAge] = useState(25);
+
+
+  // const handleClick = (e) => {
+  //   setName('luigi');
+  //   setAge(35);
+  // }
+
+  // testing what happens in this 
+  // const handleClickAgain = (name, e) => {
+  //   console.log('hello ' + name, e.target);
+  // }
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
   }
-
 
     return (
       <div className="home">
-        <h2>Homepage</h2>
-        <button onClick={handleClick}>Click me</button>
+        <BlogList blogs={blogs} title="Wasssap" handleDelete={handleDelete} />
+        {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="U wot m8" /> */}
+        {/* filter through the array and return to me each case whereby the author's name is 'mario' */}
+
+        {/* <h2>Homepage</h2>
+        <p>{name} is {age} years old</p>
+        <button onClick={handleClick}>Click me</button> */}
+        {/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
       </div>
     );
   }
