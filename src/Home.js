357449f4bf85ab910095efa2e486473243518bb9
Mahier <mahier.sydow8@gmail.com>
2022-01-11 15:16:27 +0900

    added in a home.js/navbar.js file and started creating styles

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+
+  const handleClick = () => {
+    console.log('hello, ninjas');
+  }
+
+
+    return (
+      <div className="home">
+        <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+      </div>
+    );
+  }
+   
+  export default Home;
