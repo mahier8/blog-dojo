3a976a8af76df0edfd052d588e83ca98e76697c5
Mahier <mahier.sydow8@gmail.com>
2022-01-19 14:24:24 +0900

    making a post request

--- src/Create.js
+++ src/Create.js
@@ -4,13 +4,23 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isPending, setisPending] = useState(false)
 
 const handleSubmit = (e) => {
     e.preventDefault();
+    const blog = {title, body, author};
 
-    const blog = {title, body, author}
+    setisPending(true);
 
-    console.log(blog);
+    // use fetch api with same (url) endpoint
+    fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+    }).then(() => {
+        console.log('new blog added');
+        setisPending(false);
+    })
 }
 
     return ( 
@@ -38,7 +48,10 @@ const handleSubmit = (e) => {
                     <option value="mario">mario</option>
                     <option value="luigi">luigi</option>
                 </select>
-                <button>Add blog</button>
+                {!isPending ? <button>Add blog</button> : <button disabled>Adding blog...</button>}
+                {/* can use the below, or the terniary operator as i did above */}
+                {/* {!isPending && <button>Add blog</button>}
+                {isPending && <button>no blog</button>} */}
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
